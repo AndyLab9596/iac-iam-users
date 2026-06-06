24d81be7284ec1cb4106c6010a3e615c3b294325
mac <thienvy95@gmail.com>
2026-06-06 17:45:47 +0700

    feat: wrap up

--- users.tf
+++ users.tf
@@ -1,5 +1,8 @@
 locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+  users_map = {
+    for user_config in local.users_from_yaml : user_config.username => user_config.roles
+  }
 }
 
 resource "aws_iam_user" "users" {
@@ -21,7 +24,21 @@ resource "aws_iam_user_login_profile" "users" {
   }
 }
 
+resource "local_file" "iam_users_csv" {
+  filename = "${path.module}/iam_users.csv"
+  content = join("\n", concat(
+    ["username,password"],
+    [
+      for username, profile in aws_iam_user_login_profile.users : "${username},${profile.password}"
+    ]
+  ))
+}
+
 output "passwords" {
   sensitive = true
   value     = { for user, user_login in aws_iam_user_login_profile.users : user => user_login.password }
+}
+
+output "users_map" {
+  value = local.users_map
 }
